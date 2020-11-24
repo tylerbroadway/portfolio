349ef8f006802725666cdf7907bc07b2dd6c4225
tyler broadway <tbroadway96@gmail.com>
2020-11-23 22:53:21 -0600

    working on banner

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -24,7 +24,7 @@ const NavBar = () => {
 
   return (
     <div className={classes.root}>
-      <AppBar position="fixed">
+      <AppBar position="static" className="header">
         <Toolbar>
           {/* <IconButton
             edge="start"
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,8 +3,24 @@ import NavBar from "../Nav";
 
 const HomePage = () => {
   return (
-    <div>
+    <div
+      className="home"
+      style={{
+        width: "100%",
+        margin: "auto",
+      }}
+    >
       <NavBar />
+      <div className="banner">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHj9dWR0-p1oTrDkBj6jjaz6ejROWiwmm5bg&usqp=CAU"
+          alt="avatar"
+          className="avatar"
+        />
+        <div className="banner-text">
+          <h1>Full Stack Software Engineer</h1>
+        </div>
+      </div>
     </div>
   );
 };
