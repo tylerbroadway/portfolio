b94478d15c4f5811e90a7ae82b1adbd9ce241918
tyler broadway <tbroadway96@gmail.com>
2020-11-25 18:45:30 -0600

    Modified home page & changed image

---
+++ src/components/projects/ProjectsPage.js
@@ -0,0 +1,12 @@
+import NavBar from "../Nav";
+
+const ProjectsPage = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Hello, World</h1>
+    </div>
+  );
+};
+
+export default ProjectsPage;
