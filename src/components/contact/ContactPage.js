e4372ac8681b06fb10264d5023b4f0fa01debe2c
tyler broadway <tbroadway96@gmail.com>
2020-11-26 22:41:59 -0600

    Added contact page route

---
+++ src/components/contact/ContactPage.js
@@ -0,0 +1,12 @@
+import Nav from "../Nav";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Nav />
+      <h1 style={{ marginTop: "5rem" }}>Contact Me</h1>
+    </div>
+  );
+};
+
+export default ContactPage;
