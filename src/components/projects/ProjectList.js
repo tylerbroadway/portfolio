c776521c4823c3e1a3972cc31daf534d322c8ade
tyler broadway <tbroadway96@gmail.com>
2020-11-26 22:24:28 -0600

    Change GoL img

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -4,7 +4,7 @@ const ProjectList = () => {
   const myProjects = [
     {
       title: "Conway's Game of Life",
-      img: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
+      img: "https://pi.math.cornell.edu/~lipa/mec/banner.png",
       description: "I built a cool app that does cool stuff",
       tech: ["JavaScript", "React", "Styled Components"],
       repo: "https://github.com/tylerbroadway/portfolio",
